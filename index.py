52a2fc7886b923fb6806869fbebf62f036c44c8e
CloverMidori <youhuid07@gmail.com>
2020-10-28 20:22:49 +0900

    あ

--- index.py
+++ index.py
@@ -22,7 +22,7 @@ async def on_message(message):
   if not message.content.startsWith(config.prefix) and message.author == client.user: 
     return
 
-  args = message.content[len(config.prefix):].strip().split(/ +/)
+  args = message.content[len(config.prefix):].strip().split(" ")
 
   if message.content.startswith('cm!mecab'):
     await mecab(message, args)

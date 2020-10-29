737b0ecd3396f40bc116503460e38a019e11d9e7
Clover_Midori <64039436+CloverMidori@users.noreply.github.com>
2020-10-29 21:35:33 +0900

    update index.py

--- index.py
+++ index.py
@@ -24,25 +24,25 @@ from tensorflow.python.platform import gfile
 
 tf.app.flags.DEFINE_float("learning_rate", 0.5, "Learning rate.")
 tf.app.flags.DEFINE_float("learning_rate_decay_factor", 0.99,
-  "Learning rate decays by this much.")
+                          "Learning rate decays by this much.")
 tf.app.flags.DEFINE_float("max_gradient_norm", 5.0,
-  "Clip gradients to this norm.")
+                          "Clip gradients to this norm.")
 tf.app.flags.DEFINE_integer("batch_size", 4,
-  "Batch size to use during training.")
+                            "Batch size to use during training.")
 tf.app.flags.DEFINE_integer("size", 256, "Size of each model layer.")
 tf.app.flags.DEFINE_integer("num_layers", 2, "Number of layers in the model.")
-tf.app.flags.DEFINE_integer("in_vocab_size", 12500, "input vocabulary size.")
-tf.app.flags.DEFINE_integer("out_vocab_size", 12500, "output vocabulary size.")
+tf.app.flags.DEFINE_integer("in_vocab_size", 500, "input vocabulary size.")
+tf.app.flags.DEFINE_integer("out_vocab_size", 500, "output vocabulary size.")
 tf.app.flags.DEFINE_string("data_dir", "./datas", "Data directory")
 tf.app.flags.DEFINE_string("train_dir", "./datas", "Training directory.")
 tf.app.flags.DEFINE_integer("max_train_data_size", 0,
-  "Limit on the size of training data (0: no limit).")
+                            "Limit on the size of training data (0: no limit).")
 tf.app.flags.DEFINE_integer("steps_per_checkpoint", 100,
-  "How many training steps to do per checkpoint.")
+                            "How many training steps to do per checkpoint.")
 tf.app.flags.DEFINE_boolean("decode", False,
-  "Set to True for interactive decoding.")
+                            "Set to True for interactive decoding.")
 tf.app.flags.DEFINE_boolean("self_test", False,
-  "Run a self-test if this is set to True.")
+                            "Run a self-test if this is set to True.")
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -74,22 +74,20 @@ def read_data(source_path, target_path, max_size=None):
 
 def create_model(session, forward_only):
   model = seq2seq_model.Seq2SeqModel(
-      FLAGS.in_vocab_size, FLAGS.out_vocab_size, _buckets,
-      FLAGS.size, FLAGS.num_layers, FLAGS.max_gradient_norm, FLAGS.batch_size,
-      FLAGS.learning_rate, FLAGS.learning_rate_decay_factor,
-      forward_only=forward_only)
+      FLAGS.in_vocab_size, FLAGS.out_vocab_size, _buckets,                           
+      FLAGS.size, FLAGS.num_layers, FLAGS.max_gradient_norm, FLAGS.batch_size,      
+      FLAGS.learning_rate, FLAGS.learning_rate_decay_factor,                       
+      forward_only=forward_only)                                                    
 
   ckpt = tf.train.get_checkpoint_state(FLAGS.train_dir)
   #if ckpt and gfile.Exists(ckpt.model_checkpoint_path):
     #add
-  if not os.path.isabs(ckpt.model_checkpoint_path):
-    ckpt.model_checkpoint_path= os.path.abspath(os.path.join(os.getcwd(), ckpt.model_checkpoint_path))
-    #so far
-    print("Reading model parameters from %s" % ckpt.model_checkpoint_path)
-    model.saver.restore(session, ckpt.model_checkpoint_path)
+  if ckpt and gfile.Exists(ckpt.model_checkpoint_path):                              
+    print("Reading model parameters from %s" % ckpt.model_checkpoint_path)          
+    model.saver.restore(session, ckpt.model_checkpoint_path)                        
   else:
     print("Created model with fresh parameters.")
-    session.run(tf.initialize_all_variables())
+    session.run(tf.initialize_all_variables())                                      
   return model
 
 #学習済みモデル、語彙ファイルを読み込み

dae5718a280b974c70bd22de94fccd0afce1ceaa
CloverMidori <youhuid07@gmail.com>
2020-10-30 20:39:59 +0900

    あ

--- juman.py
+++ juman.py
@@ -1,109 +1,45 @@
 import numpy as np
 import csv
 import glob
-import re
+import pickle
 
-def make_data(fname,data2) :
-    f = open(fname, 'r')
+#*******************************************************************************
+#                                                                              *
+#   品詞分解                                                                   *
+#                                                                              *
+#*******************************************************************************
+def decomposition(file, jumanpp) :
+    f=open(file, 'r')
     df1 = csv.reader(f)
-    data1 = [ v for v in df1]
+    data = [ v for v in df1]
+    print('number of rows :', len(data))
 
-    print(len(data1))
-    #ファイル読み込み
-    text = ''
-    for i in range(0,len(data1)):
-        if len(data1[i]) == 0:
-            print('null')
-            continue
-
-        s = data1[i][0]
-        if s[0:5] == "％ｃｏｍ：" :
-            continue
-        if s[0]  != '＠' :
-            #不明文字をUNKに置き換え
-            s = s.replace('＊＊＊','UNK')
-            #会話文セパレータ
-            if s[0] == 'F' or s[0] == 'M':
-                s = 'SSSS'+s[5:]
-            if s[0:2] == 'Ｘ：':
-                s = 'SSSS'+s[2:]
-
-            s = re.sub('F[0-9]{3}',"UNK",s)
-            s = re.sub('M[0-9]{3}',"UNK",s)
-            s = s.replace("＊","")
+    parts = []
+    for i in range(len(data)) :
+        if len(data[i][0].encode('utf-8')) <= 4096 :
+            result = jumanpp.analysis(data[i][0])
         else :
+            print(i, ' skip')
             continue
-
-        while s.find("（") != -1 :
-            start_1 = s.find("（")
-            if s.find("）") != -1 :
-                end_1 = s.find("）")
-                if start_1 >= end_1 :
-                    s = s.replace(s[end_1],"")
-                else :
-                    s = s.replace(s[start_1:end_1+1],"")
-                if len(s) == 0 :
-                    continue
-            else :
-                s=s[0:start_1]
-
-        while s.find("［") != -1 :
-            start_2 = s.find("［")
-            if s.find("］") != -1 :
-                end_2=s.find("］")
-                s=s.replace(s[start_2:end_2+1],"")
-            else :
-                s=s[0:start_2]    
-
-        while s.find("＜") != -1 :
-            start_3 = s.find("＜")
-            if s.find("＞") != -1 :
-                end_3 = s.find("＞")
-                s = s.replace(s[start_3:end_3+1],"")
-            else :
-                s = s[0:start_3] 
-
-        while s.find("【") != -1 :
-            start_4 = s.find("【")
-            if s.find("】") != -1 :
-                end_4 = s.find("】")
-                s = s.replace(s[start_4:end_4+1],"")
-            else :
-                s = s[0:start_4] 
-
-        #いろいろ削除したあとに文字が残っていたら出力文字列に追加
-        if s != "\n" and s != "SSSS" :
-            text += s
-    #セパレータごとにファイル書き込み
-    text =text[4:]
-    while text.find("SSSS") != -1 :
-        end_s = text.find("SSSS")
-        t = text[0:end_s]
-        #長い会話文を分割
-        if end_s > 100 :
-            while len(t) > 100 :
-                if t.find("。") != -1 :
-                    n_period = t.find("。")
-                    data2.append("SSSS"+t[0:n_period+1])
-                    t = t[n_period+1:]
-                else :
-                    break
-        data2.append("SSSS"+t)
-        text = text[end_s+4:]
-    f.close()
-    return
-
-file_list = glob.glob('nucc/*')
+        if i % 5000 == 0 :
+            print(i)
+    return parts
+    
+#*******************************************************************************
+#                                                                              *
+#   メイン処理                                                                 *
+#                                                                              *
+#*******************************************************************************
+from pyknp import Juman
+jumanpp = Juman()
+file_list=glob.glob('corpus/*')
+file_list.sort()
 print(len(file_list))
 
-data2=[]
-for j in range(0,len(file_list)) :
+parts_list = []
+for j in range(len(file_list)) :
     print(file_list[j])
-    make_data(file_list[j],data2)
+    parts_list += decomposition(file_list[j], jumanpp)
 
-#ファイルセーブ
-f = open('corpus/corpus_MEIDAI.txt','w')
-for i in range(0,len(data2)):
-    f.write(str(data2[i])+"\n")
-f.close()
-print(len(data2))
+with open('parts_list.pickle', 'wb') as f :    
+    pickle.dump(parts_list , f)  

b2d083e894de131998fdc8dec2959bd7041a9b19
CloverMidori <youhuid07@gmail.com>
2020-10-31 16:34:07 +0900

    あ

--- juman.py
+++ juman.py
@@ -34,10 +34,8 @@ def decomposition(file, jumanpp) :
             print(i, ' skip')
             continue
         for mrph in result.mrph_list():
-            if 'REQ' in mrph.midasi:
-              print(mrph.midasi)
             parts += mrph.midasi
-        if i % 5000 == 0 :
+        if i % 10 == 0 :
             print(i)
     return parts
 #*******************************************************************************

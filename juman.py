095b59c896e5d99ca0314ca4e4faf2c8f3e00bb0
CloverMidori <youhuid07@gmail.com>
2020-10-31 09:20:05 +0900

    あ

--- juman.py
+++ juman.py
@@ -8,6 +8,18 @@ import pickle
 #   単語補正                                                                   *
 #                                                                              *
 #*******************************************************************************
+def modification(word) :
+    if len(word) > 7 and word[:7] == 'SSSSUNK' :
+        modified = ['SSSS', word[7:]]
+    elif len(word) > 4 and word[:4] == 'SSSS' :
+        modified = ['SSSS', word[4:]]
+    elif word == 'UNKUNK' :
+        modified = ['UNK']
+    elif len(word) > 3 and word[:3] == 'UNK' :
+        modified = ['UNK', word[3:]]
+    else :
+        modified = [word]
+    return modified
 
 #*******************************************************************************
 #                                                                              *
@@ -17,23 +29,21 @@ import pickle
 def decomposition(file, jumanpp) :
     f=open(file, 'r')
     df1 = csv.reader(f)
-    data = [df1]
+    data = [ v for v in df1]
     print('number of rows :', len(data))
 
     parts = []
     for i in range(len(data)) :
         if len(data[i][0].encode('utf-8')) <= 4096 :
-            word = data[i][0].replace(' ', '　')
-            result = jumanpp.analysis(word)
+            result = jumanpp.analysis(data[i][0])
         else :
             print(i, ' skip')
             continue
         for mrph in result.mrph_list():
-            parts += mrph.midasi
+            parts += modification(mrph.midasi)
         if i % 5000 == 0 :
             print(i)
     return parts
-    
 #*******************************************************************************
 #                                                                              *
 #   メイン処理                                                                 *
